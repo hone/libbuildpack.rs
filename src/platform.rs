a68bc9da770e657af4873e80b0db14dae9191b0c
Terence Lee <hone02@gmail.com>
2019-08-07 19:35:44 -0500

    add set_env for Platform

--- src/platform.rs
+++ src/platform.rs
@@ -28,32 +28,79 @@ impl Platform {
 
         Ok(Self { env: env })
     }
+
+    pub fn set_env(&self) {
+        for (key, val) in self.env.vars_os() {
+            std::env::set_var(key, val);
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    use std::path::PathBuf;
     use std::result::Result;
 
     use failure::Error;
     use tempdir::TempDir;
 
-    #[test]
-    fn it_reads_env() -> Result<(), Error> {
-        let platform_dir = TempDir::new("platform")?;
-        let env_dir = platform_dir.path().join("env");
+    struct Setup {
+        _temp_dir: TempDir,
+        pub env_dir: PathBuf,
+        pub platform_dir: PathBuf,
+    }
+
+    fn setup() -> Result<Setup, Error> {
+        let tmp_dir = TempDir::new("platform")?;
+        let platform_dir = tmp_dir.path().join("");
+        let env_dir = platform_dir.join("env");
         fs::create_dir_all(&env_dir)?;
 
-        fs::write(env_dir.join("FOO"), "BAR")?;
-        fs::write(env_dir.join("BAR"), "BAZ")?;
+        Ok(Setup {
+            _temp_dir: tmp_dir,
+            env_dir: env_dir,
+            platform_dir: platform_dir,
+        })
+    }
 
-        let platform = Platform::new(platform_dir);
+    #[test]
+    fn it_reads_env() -> Result<(), Error> {
+        let setup = setup()?;
+
+        fs::write(setup.env_dir.join("FOO"), "BAR")?;
+        fs::write(setup.env_dir.join("BAR"), "BAZ")?;
+
+        let platform = Platform::new(setup.platform_dir);
 
         assert!(platform.is_ok());
         if let Ok(platform) = platform {
             assert_eq!(platform.env.var("FOO").unwrap(), "BAR");
             assert_eq!(platform.env.var("BAR").unwrap(), "BAZ");
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn it_sets_env() -> Result<(), Error> {
+        let setup = setup()?;
+        fs::write(setup.env_dir.join("FOO"), "BAR")?;
+        fs::write(setup.env_dir.join("BAR"), "BAZ")?;
+
+        let platform = Platform::new(setup.platform_dir)?;
+        let old_vars = std::env::vars();
+
+        platform.set_env();
+        assert_eq!(std::env::var("FOO")?, "BAR");
+        assert_eq!(std::env::var("BAR")?, "BAZ");
+
+        for (key, _) in platform.env.vars_os() {
+            std::env::remove_var(key);
+        }
+        for (key, value) in old_vars {
+            std::env::set_var(key, value);
         }
 
         Ok(())
